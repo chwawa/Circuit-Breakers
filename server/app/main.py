828fd54a10fa7edec05bb347003d440ae33900a5
Chelsea Wang <chelseawang9@gmail.com>
2026-01-17 14:37:37 -0500

    3d model stuff

--- server/app/main.py
+++ server/app/main.py
@@ -2,18 +2,19 @@
 Backend - API Server
 Connection point between frontend and backend services
 """
-import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import requests
+import time
+import os
 import asyncio
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-from llm_parser import parse_llm_text
+from fastapi.responses import JSONResponse, PlainTextResponse
+from .llm_parser import parse_llm_text
 import httpx
 from pydantic import BaseModel
-
 
 MESHY_API_KEY = os.getenv("MESHY_API_KEY")
 
@@ -30,6 +31,14 @@ app.add_middleware(
 class ChatRequest(BaseModel):
     prompt: str
     track_positions: bool = True
+
+class ImageRequest(BaseModel):
+    image_url: str
+
+@app.exception_handler(Exception)
+async def debug_exception_handler(request: Request, exc: Exception):
+    print("ERROR:", exc)
+    return PlainTextResponse(str(exc), status_code=400)
 
 """ Placeholder function to call an LLM API """
 async def call_llm(prompt: str) -> str:
@@ -76,40 +85,53 @@ MESHY_HEADERS = {
 }
 
 @app.post("/generate-3d")
-async def generate_3d(image: UploadFile = File(...)):
+async def generate_3d(req: ImageRequest):
     async with httpx.AsyncClient(timeout=60) as client:
 
-        # 1. Create Meshy image-to-3D task
-        files = {
-            "image": (image.filename, await image.read(), image.content_type)
+        # 1. Generate a preview model and get the task ID
+        image_url = req.image_url
+        print("Received image url:", image_url[0:10])
+        payload = {
+            "image_url": image_url
         }
 
-        create_task = await client.post(
+        generate_preview_response = requests.post(
             "https://api.meshy.ai/openapi/v1/image-to-3d",
             headers=MESHY_HEADERS,
-            files=files
+            json=payload,
         )
 
-        task_data = create_task.json()
-        task_id = task_data["result"]["task_id"]
+        generate_preview_response.raise_for_status()
+        print("preview response: ", generate_preview_response.json())
 
-        # 2. Poll task
+        preview_task_id = generate_preview_response.json()["result"]
+
+        print("Preview task created. Task ID:", preview_task_id)
+
+        # 2. Poll the preview task status until it's finished
+        preview_task = None
         while True:
-            await asyncio.sleep(3)
-
-            poll = await client.get(
-                f"https://api.meshy.ai/openapi/v1/tasks/{task_id}",
-                headers=MESHY_HEADERS
+            preview_task_response = requests.get(
+                f"https://api.meshy.ai/openapi/v1/image-to-3d/{preview_task_id}",
+                headers=MESHY_HEADERS,
             )
 
-            data = poll.json()
-            status = data["result"]["status"]
+            preview_task_response.raise_for_status()
 
-            if status == "succeeded":
-                return {
-                    "glbUrl": data["result"]["outputs"]["glb"]
-                }
-
-            if status == "failed":
-                return {"error": "Meshy generation failed"}
+            preview_task = preview_task_response.json()
+        
             
+            if preview_task:
+                print(preview_task)
+                return preview_task["model_urls"]["glb"]
+
+        # 3. Download the preview model in glb format
+
+        # preview_model_url = preview_task["model_urls"]["glb"]
+
+        # preview_model_response = requests.get(preview_model_url)
+        # preview_model_response.raise_for_status()
+
+        # with open("preview_model.glb", "wb") as f:
+        #     f.write(preview_model_response.content)
+        # print("Preview model downloaded.")
